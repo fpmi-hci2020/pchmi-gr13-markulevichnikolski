6af48c3aae9d165c84b721d96d00ee93ff86bf5a
sergei99 <thisisbadfuck@mail.ru>
2020-12-25 20:40:21 +0300

    added profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import history from '../../history';
+import { FaHome } from "react-icons/fa"
 
 import './styles.scss';
 
 const Profile = () => {
+    const userDataProfile = JSON.parse(localStorage.getItem('userData'));
+
+    const goHome = () =>
+    {
+        history.push('/home')
+    }
+
     return (
-        <div>
-            Profile
+        <div className = "profile">
+            <div className = "color-height">
+                <FaHome className = "home-button" onClick = {goHome}/>
+                <div className = "profile-logo">Profile</div>
+            </div>
+            <div className = "color-bottom">
+                <h2 className = "first-last-name">{userDataProfile.lastName} {userDataProfile.firstName}</h2>
+            </div>
         </div>
     );
 };
