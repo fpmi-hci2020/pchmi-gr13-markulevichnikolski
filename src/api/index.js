f9653f5f1224cb93d9d807b23c3daa47639e7466
VadimRijiiBGU <nikolskivadim@gmail.com>
2020-12-26 11:27:46 +0300

    feat: changed url to heroku app

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3030'
+    // baseURL: 'http://localhost:3030'
+    baseURL: 'https://glacial-lowlands-06396.herokuapp.com/'
 });
 
 instance.interceptors.request.use(config => {
